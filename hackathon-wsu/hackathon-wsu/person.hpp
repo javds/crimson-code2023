01eb540e51b43929c9ea147c0dffe66a16b51e88
justanmeemman <45608802+justanmeemman@users.noreply.github.com>
2023-02-18 11:39:42 -0800

    push personwork

--- hackathon-wsu/hackathon-wsu/person.hpp
+++ hackathon-wsu/hackathon-wsu/person.hpp
@@ -7,11 +7,32 @@ class Person
 {
 public:
 	Person();
+
 	~Person();
 
 	Person(std::string name, int day, int month, int year, int wsuid);
 
-	
+	std::string getName();
+
+	int getDayBirth();
+
+	int getMonthYear();
+
+	int getYearBirth();
+
+	int getWsuId();
+
+	void setName(std::string setName);
+
+	void setDayBirth(int dateBirth);
+
+	void setMonthBirth(int MonthBirth);
+
+	void setYearBirth(int setYear);
+
+	void setWsuid(int setWsuid);
+
+
 
 
 private:
