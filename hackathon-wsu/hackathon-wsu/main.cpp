911d1095a527c680d8040a1260884abbb4b09d64
justanmeemman <45608802+justanmeemman@users.noreply.github.com>
2023-02-18 16:42:22 -0800

    stuff

--- hackathon-wsu/hackathon-wsu/main.cpp
+++ hackathon-wsu/hackathon-wsu/main.cpp
@@ -4,9 +4,22 @@
 int main(void) 
 {
 
-	Person dude("things", 0, 0, 0, 0);
-
+	networking net(45000, "127.0.0.1", "test");
+	net.start();
+	char msg[4096] = "this a test";
+	char receive[4096];
+	
+	while (true) 
+	{
+		Sleep(1000);
+		memcpy(net.sendData, msg, 4096);
+		net.netSend();
+		std::cout << "sent\n";
+		memcpy(receive, net.netReceive(), 4096);
+	}
 	
 
+	
+	net.shutdown();
 	return 0;
 }
